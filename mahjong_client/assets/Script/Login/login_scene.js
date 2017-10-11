8bff3eb9791ec7a6205e928e602d6f99f7a20ae6
miemie <2477887557@qq.com>
2017-10-11 11:56:40 +0800

    修改的loginsence

--- mahjong_client/assets/Script/Login/login_scene.js
+++ mahjong_client/assets/Script/Login/login_scene.js
@@ -7,6 +7,11 @@ cc.Class({
             type: cc.Node,
             tooltip: "登录的按钮",
         },
+        CheckBox: {
+            default: null,
+            type: cc.Node,
+            tooltip: "用户协议",
+        },
     },
 
     // use this for initialization
@@ -32,7 +37,19 @@ cc.Class({
             "did": "39d394f7-db13-4768-a4da-40e18273d7a8",
             "code": "6SDF4ASD4GFAS4FG5ASD5F5Dsdf"
         });
-        // cc.dd.sceneMgr.runScene(cc.dd.sceneID.HALL_SCENE);
+        cc.dd.sceneMgr.runScene(cc.dd.sceneID.HALL_SCENE);
+    },
+    clickCheckBoxToggle(event, custom) {
+        var temp = this.CheckBox.getComponent(cc.dd.CheckBox);
+        cc.log(temp.isChecked);
+        var tempbtn = this.BtnLogin.getComponent(cc.dd.BtnLogin);
+        log(tempbtn.interactable);
+        if (temp.isChecked) {
+            cc.log(temp.isChecked);
+            tempbtn.interactable = true;
+        }else{
+            tempbtn.interactable = false;
+        }
     },
     // 设置登录按钮的状态
     setBtnLoginState(state) {
